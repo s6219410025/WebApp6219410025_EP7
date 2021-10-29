6ffad8c3dd551e5719913aba9dde990f612ceac8
s6219410025 <s6219410025@sau.ac.th>
2021-10-29 17:18:36 +0700

    V.3 Do.zzzz

---
+++ js/scrip9.js
@@ -0,0 +1,38 @@
+console.log('Hi js 9 ...')
+
+function showName(name){
+    console.log('สวัสดี' + name);
+}
+
+showName('แมว') //เรียกใช้ฟังก์ชัน call function
+showName('มด')
+
+
+function calSum(a , b){
+    console.log('a + b ได้ค่าเป็น ')
+    return a + b;
+}
+
+console.log(calSum(10, 20) )
+
+//alert( calSum(100,99))
+
+let test1 =  () => {
+    console.log('Hello....')
+}
+
+//test1 = function () {
+//    console.log('Hello....')
+//}
+
+//let test2 = function (number) {
+//    console.log('Hi....' + number,)
+//}
+
+let test2 = (number) => {
+    console.log('Hi....' + number,)
+}
+
+test1()
+test1()
+test2()
